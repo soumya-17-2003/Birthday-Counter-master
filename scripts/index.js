0bffdaf1a391db737e0c5ee1ef8c491424549784
Soumya  Ranjan Mohanty <soumyalpu17@gmail.com>
2025-05-04 01:11:26 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'May 2, 2025',
+  birthdate: 'May 4, 2025',
   name: "Tanisha"
 };
 
